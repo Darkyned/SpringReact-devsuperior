f51ebc34bb9533f4b63ba8040a53dd10f7fd7d2d
Samuel Pinheiro <samuelpinheirocoutinholopes321@gmail.com>
2022-07-16 17:17:47 -0300

    API test

---
+++ backend/src/main/java/com/superiordev/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.superiordev.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.superiordev.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
